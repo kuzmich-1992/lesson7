4c9876b9a5d8d1f1cf15bcab28311adc461b8c6a
Dima <kuzmichman92@gmail.com>
2020-05-21 01:03:17 +0300

    config_to_devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  
+  def after_sign_in_path_for(resource)
+    current_assessment_path
+  end
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,8 +1,6 @@
-class User < ApplicationRecord
+class Model < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :first_name, uniqueness: true
-  validates :last_name, uniqueness: true
 end
