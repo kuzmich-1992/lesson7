1582f3c95c4c4fcd173d4cf7ec401e47b49ba664
Dima <kuzmichman92@gmail.com>
2020-05-23 13:21:30 +0300

    redirect to assessment do

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :questions
+  devise_for :users, controllers: {
+  	sessions:'users/sessions'
+  }
+  resources :questions, only: [:index]
   resources :users
   resources :posts
+  resources :assessments, only: [:new, :create]
 end
+ 
