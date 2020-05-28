c9c3af5f5e43bf6ee537750de0a7052031c1d289
Dima <kuzmichman92@gmail.com>
2020-05-28 06:43:58 +0300

    cocoon dont work again

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -1,15 +1,33 @@
 class AssessmentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_assessment, only: %i[show]
 
   def new
     @assessment = Assessment.new
   end
 
+  def show
+  	if @assessment.blank?
+  	  redirect_to assessment_path
+  	else
+      @questions = @assessment.questions
+    end
+  end
+
   def create
-    @assessment = Assessment.new
+    @assessment = Assessment.new(assessment_params)
+      if @assessment.save
+        redirect_to @assessment
+      else
+        render :new
+      end
   end
 
   private
+
+  def set_assessment
+  	@assessment = Assessment.find_by(id: params[:id])
+  end
 
   def assessment_params
     params.require(:assessment).permit(:title, questions_attributes:[:id, :text])
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
+require('@kollegorna/cocoon-vanilla-js')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -19,4 +20,4 @@ require("jquery")
 
 import 'cocoon';
 import 'bootstrap';
-import './bootstrap_custom.js';
+
